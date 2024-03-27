9bc1676c1f6d5c6e57a97acf2317a82973de83a0
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-03-27 14:32:23 +0900

    Update main.js

--- project/main.js
+++ project/main.js
@@ -110,7 +110,7 @@ const currentSizeSpan = document.querySelector("#current-size");
 modelSizeInput.addEventListener("input", function() {
     const newSize = parseFloat(modelSizeInput.value);
     currentModel.scale.set(clamp(newSize, -0.5, 10));
-    currentSizeSpan.textContent = "Current Size: " + newSize.toFixed(2);
+    currentSizeSpan.value = newSize;
 });
 
 	// 6, Live2Dモデルを配置する
